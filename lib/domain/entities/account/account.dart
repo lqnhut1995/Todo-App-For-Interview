0da190aeb86d2326772c351e64d469822b630354
Lam Nhut <lqnhut1995@gmail.com>
2022-10-11 17:22:04 +0700

    update files

--- lib/domain/entities/account/account.dart
+++
@@ -1,9 +0,0 @@
-/* spell-checker: disable */
-part of 'entities.dart';
-
-@freezed
-class AuthStatus with _$AuthStatus {
-  const factory AuthStatus.unknown() = Unknown;
-  const factory AuthStatus.authenticated() = Authenticated;
-  const factory AuthStatus.unauthenticated() = Unauthenticated;
-}
--- lib/domain/entities/account/entities.dart
+++
@@ -1,4 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'account.dart';
-part 'entities.freezed.dart';
